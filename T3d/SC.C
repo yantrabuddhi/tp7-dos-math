91716c8ea2523cafd9407351a581e02a0b80f786
Mandeep <mandeep.singh.bhatia@gmail.com>
2016-02-01 11:13:17 +0800

    math programs

---
+++ T3d/SC.C
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <conio.h>
+#include <graphics.h>
+//#include <math.h>
+void main()
+{
+int x=0,y,g=DETECT;
+initgraph(&g,&x,"c:\\tc\\bgi");
+
+getch();
+closegraph();
+}
